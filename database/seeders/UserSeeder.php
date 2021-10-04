67b1923baf3d6abf2e001eb90716bf23fc1bcc66
garlockhart <official.garlynugraha@gmail.com>
2021-10-04 12:26:42 +0700

    Change User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +16,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $administrator = User::create([
+            'name' => 'Administrator Articly',
+            'image' => 'default.png',
+            'email' => 'administrator@articly.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $administrator->assignRole('Administrator');
+
+        $write = User::create([
+            'name' => 'Garlockhart',
+            'image' => 'default.png',
+            'email' => 'garlockhart@articly.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $write->assignRole('Writer');
     }
 }
