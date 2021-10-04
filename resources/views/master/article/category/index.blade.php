77b04a75003d0079a71b82361c6cfeb9cebd70d1
garlockhart <official.garlynugraha@gmail.com>
2021-10-04 12:59:03 +0700

    Make Category View

---
+++ resources/views/master/article/category/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+    Category
+@endsection
+
+@section('content')
+    <section class="content-header">
+
+    </section>
+
+    <section class="content">
+
+    </section>
+@endsection 
