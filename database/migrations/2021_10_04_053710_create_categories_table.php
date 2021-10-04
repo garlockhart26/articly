9816bb0e6cdc8975be80565ef0676fe7f7f13932
garlockhart <official.garlynugraha@gmail.com>
2021-10-04 12:43:31 +0700

    Change Migrations Category

--- database/migrations/2021_10_04_053710_create_categories_table.php
+++ database/migrations/2021_10_04_053710_create_categories_table.php
@@ -15,6 +15,10 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('keywords');
+            $table->text('meta_desc');
             $table->timestamps();
         });
     }
