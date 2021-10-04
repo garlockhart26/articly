38496f3876d6955ff8b9feff8576a2c3c31734a6
garlockhart <official.garlynugraha@gmail.com>
2021-10-04 12:17:03 +0700

    Change Redirect Login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const MASTER = '/master/dashboard';
 
     /**
      * The controller namespace for the application.
