be643df7f3a464484a5901fe788eb4077124b9fd
garlockhart <official.garlynugraha@gmail.com>
2021-10-10 20:23:08 +0700

    Change View Sidebar

--- resources/views/layouts/_master/sidebar.blade.php
+++ resources/views/layouts/_master/sidebar.blade.php
@@ -13,6 +13,22 @@
                     <i class="fa fa-user"></i> <span>User</span>
                 </a>
             </li>
+            <li class="header">ARTICLE MANAGEMENT</li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user"></i> <span>Post Tag</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('post-category.index') }}">
+                    <i class="fa fa-user"></i> <span>Post Category</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user"></i> <span>Post</span>
+                </a>
+            </li>
         </ul>
     </section>
 </aside>
