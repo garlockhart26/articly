7dfc111fef598d17ed5a300c5adf1df19a6bc500
garlockhart <official.garlynugraha@gmail.com>
2021-10-14 19:55:16 +0700

    Change View Footer

--- resources/views/layouts/_master/footer.blade.php
+++ resources/views/layouts/_master/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021 - Template By <a href="https://adminlte.io">AdminLTE</a> & Develop By <a href="https://garlynugraha.com">Garly Nugraha</a>.</strong>
-</footer> 
+    <strong>Copyright &copy; 2021 - Template By <a href="https://adminlte.io">AdminLTE</a> & Develop By <a href="https://garlockhart.com">Garlockhart</a>.</strong>
+</footer>
