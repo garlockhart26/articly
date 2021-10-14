513a2a877e9dd36cb9927253dcb33dc78a803cc8
garlockhart <official.garlynugraha@gmail.com>
2021-10-14 19:09:13 +0700

    Change Web Routes

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['prefix' => 'master', 'middleware' => ['auth']], function(){
+Route::group(['prefix' => 'master', 'middleware' => ['auth']], function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
     Route::group(['middleware' => ['role:Administrator']], function () {
